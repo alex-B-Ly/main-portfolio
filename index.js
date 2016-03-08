42c238238ce3cf87ff2469a75b2b25190d47b2ec
Alex Ly <alexly@Alexs-MBP.home>
2016-03-07 22:56:51 -0500

    added skills to mockups and handlebars for templating

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var handle = require('express-handlebars');
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// MIDDLEWARE - HANDLEBARS
+app.engine('handlebars', handle({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// ROUTES
 var routes = require('./controllers/router.js');
 app.use('/', routes);
 
