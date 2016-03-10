a2d882ea05477345fe18ebc3be3497f2f9a42a4e
Alex Ly <alexly@Alexs-MBP.home>
2016-03-10 00:56:03 -0500

    css filepath to handlebars in main for eventual scale up.

--- controllers/router.js
+++ controllers/router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.render('home', {title: "Alex Ly - Web Developer"});
+  res.render('home', {title: "Alex Ly - Web Developer", css_filepath: "/static/css/style.css"});
 });
 
 module.exports = router;
