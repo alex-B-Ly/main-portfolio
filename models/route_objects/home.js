802d6860f4c4fde924082e3f196752cd164d59b1
Alex Ly <alexly@Alexs-MBP.home>
2016-03-12 02:05:16 -0500

    Passing framework links through Hbars to accomodate app that uses different framework.

--- models/route_objects/home.js
+++ models/route_objects/home.js
@@ -1,7 +1,9 @@
 var model = {
   title: "Alex Ly - Web Developer", 
   css_filepath: "/static/css/style.css",
-  js_filepath: "/static/js/functionality.js"
+  js_filepath: "/static/js/functionality.js",
+  css_framework: "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css",
+  css_framework_js:"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"
 }
 
 module.exports = model;
